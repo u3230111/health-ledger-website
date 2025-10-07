2cb5c8f96927a733a20455a718380ed0cd6e6845
u3230111 <112055247+u3230111@users.noreply.github.com>
2025-10-07 19:30:22 +1100

    Update index.html and update script.js

--- assests/js/script.js
+++ assests/js/script.js
@@ -8,8 +8,21 @@ async function getData() {
 
     const data = await response.json();
     console.log(data);
+
+    let item = data.results[0];
+
+     // 1. create a container
+    const itemContainer = document.createElement('div');
+
+    // 2. use template literals to easily embed the data in the string
+    itemContainer.innerHTML = `<h2>${item.title}</h2><p>${item.name}</p>`;
+
+    // 3. append to the page
+    objectsContainer.appendChild(itemContainer);
+
+
   } catch (error) {
     console.error(error.message);
   }
 }
-getData(); 
+getData();
